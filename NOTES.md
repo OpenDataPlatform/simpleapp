47023fe8c8b8cd7ea471b798536279a3471e8df2
SergeAlexandre <sa@broadsoftware.com>
2022-04-23 19:23:16 +0200

    Update NOTES with intellij tricks

--- NOTES.md
+++ NOTES.md
@@ -1,5 +1,53 @@
 
+
+# Initial setup
+
+To create kubconfig for spark account
 ```
 ./tools/generate_kubeconfig.sh spark-sapp-work spark ./kubeconfigs/spark-sapp-work.spark
+```
+
+
+To upload sample data set: 
+```
 ./tools/upload-data.sh minio1/spark-sapp/data/city_temperature.csv
 ```
+
+# Intellij setup
+
+```
+git clone https://github.com/OpenDataPlatform/simpleapp.git
+cd simpleapp/
+cd java
+./gradlew
+cd ../py/
+./setup/install.sh
+```
+
+## Projet init:
+
+- Start intellij
+- New project
+- Empty project
+- Don't care about 'not empty' warning
+
+## Python module
+
+- Project Structure/Platform Setting/SDK/+/Add Python SDK
+- Existing environment, navigate to our venv
+- Project structure / Modules
+- `+`
+- Import module
+- Navigate to py folder
+- Create module from existing source
+- Select python SDK
+- Uncheck django
+- Set module SDK to our python env
+
+## Java module
+
+- Project structure / Modules
+- `+`
+- Import module
+- Navigate to java folder
+- Import module from external model / Gradle
--- py/create_table.py
+++ py/create_table.py
@@ -14,11 +14,12 @@ def main():
     p = parser.parse_args()
     spark = SparkSession.builder.enableHiveSupport().getOrCreate()
     df = spark.read.options(header='True', inferSchema='True', delimiter=",").csv(p.src)
-    df.createOrReplaceTempView("_src_");
+    df.createOrReplaceTempView("_src_")
     df.show()
     spark.sql("CREATE DATABASE IF NOT EXISTS {database}".format(database=p.database))
     spark.sql("DROP TABLE IF EXISTS {database}.{table}".format(database=p.database, table=p.table))
-    spark.sql("CREATE TABLE IF NOT EXISTS {database}.{table} USING PARQUET LOCATION 's3a://{bucket}/{datamartFolder}/{table}' AS {select}".format(database=p.database, table=p.table, bucket=p.bucket, datamartFolder=p.datamartFolder, select=p.select))
+    spark.sql("CREATE TABLE IF NOT EXISTS {database}.{table} USING PARQUET LOCATION 's3a://{bucket}/{datamartFolder}/{table}' AS {select}"
+              .format(database=p.database, table=p.table, bucket=p.bucket, datamartFolder=p.datamartFolder, select=p.select))
     spark.sql("SHOW TABLES FROM {database}".format(database=p.database)).show()
     spark.sql("DESCRIBE TABLE {database}.{table}".format(database=p.database, table=p.table)).show()
     spark.sql("SELECT COUNT(*) FROM {database}.{table}".format(database=p.database, table=p.table)).show()
@@ -28,5 +29,3 @@ def main():
 
 if __name__ == '__main__':
     sys.exit(main())
-
-
